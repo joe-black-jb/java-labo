daf6deabc37d69bfe6ea34243081095d1e2d1674
joe-black-jb <joeblack3601@gmail.com>
2025-02-10 16:44:52 +0900

    quickMaster 以外の新ファイル追加

---
+++ SpringBootDemo/README.md
@@ -0,0 +1,10 @@
+## サーバ起動
+
+```sh
+# mvn コマンド
+mvn spring-boot:run
+
+# jar を直接起動
+mvn clean install
+java -jar target/{.jarファイル名}
+```
---
+++ old/ReferenceNonJDKClass.java
@@ -0,0 +1,10 @@
+package old;
+import org.apache.commons.lang3.util;;
+
+public class ReferenceNonJDKClass {
+
+    public static void main(String[] args) {
+        System.out.println(util.getTie(null));
+    }
+
+}
